4abfad760922f72c2dddf4939760e45863dfef18
Erik S. Carlsten <ecarlste@gmail.com>
2014-03-28 16:05:22 -0500

    Created initial ImageConverter class
    -- Added util folder to VS2012 project
    -- Created ImageConverter class inside util folder
    -- Added 2 constructor methods

---
+++ util/ImageConverter.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Drawing;
+
+namespace Day_or_Night.util
+{
+    class ImageConverter
+    {
+        Image imageToConvert;
+
+        public ImageConverter()
+        {
+            imageToConvert = null;
+        }
+
+        public ImageConverter(string filename)
+        {
+            imageToConvert = Image.FromFile(filename);
+        }
+    }
+}
