8fac539ca78348a3edf4be61ba7728cd167e8fa5
Erik S. Carlsten <ecarlste@gmail.com>
2014-03-28 15:43:36 -0500

    Created VS2012 Solution and Project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_or_Night
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
